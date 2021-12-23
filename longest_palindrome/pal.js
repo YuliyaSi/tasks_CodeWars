836a3215a1a75626b3789895b5365c58d7fc4b67
Yuliya Sidarenka <84760360+YuliyaSi@users.noreply.github.com>
2021-12-23 11:14:15 +0100

    Create pal.js

---
+++ longest_palindrome/pal.js
@@ -0,0 +1,26 @@
+longestPalindrome=function(s){
+ let arr = s.split(''),
+str1 = s.split('').reverse().join(''),
+subarr = [],
+substr = '',
+resultArr = [];
+  
+  if(arr.length == 0) return 0;
+  
+for(let i=0; i < arr.length; i++) {
+subarr.push(arr[i]);
+substr = subarr.join('')
+if(~str1.indexOf(substr)) {
+subarr;
+resultArr.push(subarr.join(''))
+} else {
+let j = subarr.pop()
+resultArr.push(subarr.join(''))
+subarr = []
+subarr.push(j)
+}
+}
+resultArr.map(i => i.length)
+let pal = Math.max(...resultArr.map(i => i.length))
+return pal;
+}
